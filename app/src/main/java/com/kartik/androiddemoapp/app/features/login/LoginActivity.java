a7081100ad1de68cc377ce5cef58117786b2b25c
Kartik Kumar Gujarati <kartikkumar.gujarati@rackspace.com>
2018-02-08 18:10:06 -0600

    Added a login activity screen for user to enter email and password.

---
+++ app/src/main/java/com/kartik/androiddemoapp/app/features/login/LoginActivity.java
@@ -0,0 +1,108 @@
+package com.kartik.androiddemoapp.app.features.login;
+
+import android.support.design.widget.FloatingActionButton;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+import com.kartik.androiddemoapp.R;
+import com.kartik.androiddemoapp.app.features.base.BaseActivity;
+
+import static com.kartik.androiddemoapp.utils.LogUtils.makeLogTag;
+
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends BaseActivity {
+
+    private static final String TAG = makeLogTag(LoginActivity.class);
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        // Set up the login form.
+        mEmailView = findViewById(R.id.email);
+        mPasswordView = findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        FloatingActionButton login = findViewById(R.id.login);
+        login.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+    }
+
+
+    private void attemptLogin() {
+
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return email.length() > 4;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() > 4;
+    }
+
+}
+
